9a5a72d851ff2d4aa88bef869885131a17dc583c
Rick <info@arieck.nl>
2015-05-21 02:52:02 +0200

    added registration form

--- luckylottery/views.py
+++ luckylottery/views.py
@@ -7,19 +7,17 @@ from pyramid.renderers import get_renderer
 from pyramid.security import remember, forget, authenticated_userid
 from pyramid.view import view_config, forbidden_view_config
 
-from .models import DBSession, Page
+from .models import DBSession, Ticket
 from .security import USERS
 
 
-class WikiPage(colander.MappingSchema):
-    title = colander.SchemaNode(colander.String())
-    body = colander.SchemaNode(
-        colander.String(),
-        widget=deform.widget.RichTextWidget()
-    )
+class TicketSchema(colander.MappingSchema):
+    email = colander.SchemaNode(colander.String())
+    amount = colander.SchemaNode(
+        colander.Integer(), validator=colander.Range(0, 20))
 
 
-class WikiViews(object):
+class LotteryViews(object):
     def __init__(self, request):
         self.request = request
         renderer = get_renderer("templates/layout.pt")
@@ -27,126 +25,31 @@ class WikiViews(object):
         self.logged_in = authenticated_userid(request)
 
     @reify
-    def wiki_form(self):
-        schema = WikiPage()
-        return deform.Form(schema, buttons=('submit',))
+    def registration_form(self):
+        schema = TicketSchema()
+        return deform.Form(schema, buttons=('confirm',))
 
     @reify
     def reqts(self):
-        return self.wiki_form.get_widget_resources()
+        return self.registration_form.get_widget_resources()
 
-    @view_config(route_name='wiki_view',
-                 renderer='templates/wiki_view.pt')
-    def wiki_view(self):
-        pages = DBSession.query(Page).order_by(Page.title)
-        return dict(title='Welcome to the Wiki', pages=pages)
-
-    @view_config(route_name='wikipage_add',
-                 permission='edit',
-                 renderer='templates/wikipage_addedit.pt')
-    def wikipage_add(self):
-
-        if 'submit' in self.request.params:
+    @view_config(route_name='registration',
+                 renderer='templates/registration.pt')
+    def registration(self):
+        if 'confirm' in self.request.params:
             controls = self.request.POST.items()
             try:
-                appstruct = self.wiki_form.validate(controls)
+                appstruct = self.registration_form.validate(controls)
             except deform.ValidationFailure as e:
                 # Form is NOT valid
-                return dict(title='Add Wiki Page', form=e.render())
+                return dict(title='Lottery Ticket Registration', form=e.render())
 
-            # Add a new page to the database
-            new_title = appstruct['title']
-            new_body = appstruct['body']
-            DBSession.add(Page(new_title, new_body))
+            # Add all the tickets to the database
+            email = appstruct['email']
+            for x in range(0, appstruct['amount']):
+                DBSession.add(Ticket(email))
 
-            # Get the new ID and redirect
-            page = DBSession.query(Page).filter_by(title=new_title).one()
-            new_uid = page.uid
-
-            url = self.request.route_url('wikipage_view', uid=new_uid)
+            url = self.request.route_url('registration')
             return HTTPFound(url)
 
-        return dict(title='Add Wiki Page', form=self.wiki_form.render())
-
-    @view_config(route_name='wikipage_view',
-                 renderer='templates/wikipage_view.pt')
-    def wikipage_view(self):
-        uid = int(self.request.matchdict['uid'])
-        page = DBSession.query(Page).filter_by(uid=uid).one()
-
-        return dict(page=page, title=page.title)
-
-    @view_config(route_name='wikipage_edit',
-                 permission='edit',
-                 renderer='templates/wikipage_addedit.pt')
-    def wikipage_edit(self):
-        uid = int(self.request.matchdict['uid'])
-        page = DBSession.query(Page).filter_by(uid=uid).one()
-        title = 'Edit ' + page.title
-
-        if 'submit' in self.request.params:
-            controls = self.request.POST.items()
-            try:
-                appstruct = self.wiki_form.validate(controls)
-            except deform.ValidationFailure as e:
-                return dict(title=title, page=page, form=e.render())
-
-            # Change the content and redirect to the view
-            page.title = appstruct['title']
-            page.body = appstruct['body']
-
-            url = self.request.route_url('wikipage_view', uid=uid)
-            return HTTPFound(url)
-
-        form = self.wiki_form.render(dict(
-            uid=page.uid, title=page.title, body=page.body)
-        )
-
-        return dict(page=page, title=title, form=form)
-
-    @view_config(route_name='wikipage_delete', permission='edit')
-    def wikipage_delete(self):
-        uid = int(self.request.matchdict['uid'])
-        page = DBSession.query(Page).filter_by(uid=uid).one()
-        DBSession.delete(page)
-
-        url = self.request.route_url('wiki_view')
-        return HTTPFound(url)
-
-    @view_config(route_name='login', renderer='templates/login.pt')
-    @forbidden_view_config(renderer='templates/login.pt')
-    def login(self):
-        request = self.request
-        login_url = request.route_url('login')
-        referrer = request.url
-        if referrer == login_url:
-            referrer = '/'  # never use login form itself as came_from
-        came_from = request.params.get('came_from', referrer)
-        message = ''
-        login = ''
-        password = ''
-        if 'form.submitted' in request.params:
-            login = request.params['login']
-            password = request.params['password']
-            if USERS.get(login) == password:
-                headers = remember(request, login)
-                return HTTPFound(location=came_from,
-                                 headers=headers)
-            message = 'Failed login'
-
-        return dict(
-            title='Login',
-            message=message,
-            url=request.application_url + '/login',
-            came_from=came_from,
-            login=login,
-            password=password,
-        )
-
-    @view_config(route_name='logout')
-    def logout(self):
-        request = self.request
-        headers = forget(request)
-        url = request.route_url('wiki_view')
-        return HTTPFound(location=url,
-                         headers=headers)
+        return dict(title='Lottery Ticket Registration', form=self.registration_form.render())
